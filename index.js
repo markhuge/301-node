e346503cc9e4e61987725aaad39d2104ac4e54ec
markhuge <mark@segfawlt.net>
2015-01-21 11:33:26 -0800

    intial codebase

---
+++ index.js
@@ -0,0 +1,14 @@
+var http   = require('http'),
+    env    = process.env,
+    port   = env.port || 8080,
+    proto  = env.REDIRECT_PROTOCOL || 'https',
+    domain = env.REDIRECT_DOMAIN || '127.0.0.1';
+
+http.createServer(handler).listen(port);
+
+function handler (req, res) {
+  var header = proto + '://' + domain + req.url
+  console.log("redirecting to:", header);
+  res.writeHead(302, { Location: header });
+  res.end();
+}
