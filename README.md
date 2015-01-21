1e5358b712985bfd667b9857fb460585bf66dadc
markhuge <mark@segfawlt.net>
2015-01-21 13:35:36 -0800

    bin/ and bump

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # 301
 A super simple HTTP redirection server
 
-Don't use me yet. I'm being released very quickly for an internal deployment. The useful version of me will be released in a few hours.
+# Don't use me yet. I'm being released very quickly for an internal deployment. The useful version of me will be released in a few hours.
+
+## Install
+
+`npm install -g 301`
+
+## Usage 
+
+`user@host:$ 301`
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var http   = require('http'),
-    env    = process.env,
-    port   = env.port || 8080,
-    proto  = env.REDIRECT_PROTOCOL || 'https',
-    domain = env.REDIRECT_DOMAIN || '127.0.0.1';
+var http   = require('http');
 
-http.createServer(handler).listen(port);
+module.exports = function (opts) {
+  var opts = opts || {};
+  var proto  = opts.proto || 'http',
+      port   = opts.port || 8080,
+      domain = opts.domain || '127.0.0.1';
+  http.createServer(handler).listen(port);
+  console.log("Starting redirect server on", port);
 
-function handler (req, res) {
-  var header = proto + '://' + domain + req.url
-  console.log("redirecting to:", header);
-  res.writeHead(301, { Location: header });
-  res.end();
+  function handler (req, res) {
+    var header = proto + '://' + domain + req.url
+    console.log("redirecting to:", header);
+    res.writeHead(301, { Location: header });
+    res.end();
+  }
 }
