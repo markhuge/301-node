0082543b1f850b049d439143db62e8d672d2fa86
markhuge <mark@segfawlt.net>
2015-01-22 17:20:24 -0800

    now with moar healthcheck. closes #1

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # 301
 A super simple HTTP redirection server
 
-
 ## Install
 
 `npm install -g 301`
@@ -15,3 +14,4 @@ A super simple HTTP redirection server
 - port - port service will listen on (default 8080)
 - domain - domain requests will redirect to (default 127.0.0.1)
 - proto - protocol HTTP or HTTPS (default HTTP)
+- health - optional port on which to listen for a health check (default is disabled). Handy for load balancers that will only accept a "200" response to keep 301 instance(s) in load.
--- index.js
+++ index.js
@@ -4,14 +4,20 @@ module.exports = function (opts) {
   var opts = opts || {};
   var proto  = opts.proto || 'http',
       port   = opts.port || 8080,
+      health = opts.health || undefined,
       domain = opts.domain || '127.0.0.1';
+ 
   http.createServer(handler).listen(port);
   console.log("Starting redirect server on", port);
 
+  if (health) http.createServer(healthCheck).listen(health);
+  
   function handler (req, res) {
     var header = proto + '://' + domain + req.url
     console.log("redirecting to:", header);
     res.writeHead(301, { Location: header });
     res.end();
   }
+
+  function healthCheck (req, res) { res.end(); }
 }
